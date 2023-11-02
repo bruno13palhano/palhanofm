e7a0a79852f2c202205b96fda548bdcf4a533fe3
bruno13palhano <bruno13palhano@gmail.com>
2023-11-02 15:07:00 -0300

    Add PlaybackService class

---
+++ app/src/main/java/com/bruno13palhano/palhanofm/mediaplayer/PlaybackService.kt
@@ -0,0 +1,123 @@
+package com.bruno13palhano.palhanofm.mediaplayer
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.app.TaskStackBuilder
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSessionService
+import com.bruno13palhano.palhanofm.MainActivity
+import com.bruno13palhano.palhanofm.R
+
+@UnstableApi
+class PlaybackService : MediaSessionService() {
+    private var mediaSession: MediaSession? = null
+    private lateinit var player: ExoPlayer
+
+    companion object {
+        private const val NOTIFICATION_ID = 123
+        private const val CHANNEL_ID = "palhano_fm_session_notification_channel_id"
+    }
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? =
+        mediaSession
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        if (!player.playWhenReady) {
+            stopSelf()
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        initializeSessionAndPlayer()
+        setListener(MediaSessionServiceListener())
+    }
+
+    override fun onDestroy() {
+        mediaSession?.run {
+            player.release()
+            release()
+            mediaSession = null
+        }
+        super.onDestroy()
+    }
+
+    private fun initializeSessionAndPlayer() {
+        player = ExoPlayer.Builder(this)
+            .setAudioAttributes(AudioAttributes.DEFAULT, true)
+            .build()
+
+        val mediaItem = MediaItem.fromUri("https://stm002.smghosting.com.br:7332/stream/")
+        player.addMediaItem(mediaItem)
+        player.prepare()
+        player.play()
+
+        val sessionActivityPendingIntent =
+            TaskStackBuilder.create(this).run {
+                addNextIntent(Intent(this@PlaybackService, MainActivity::class.java))
+
+                getPendingIntent(0, FLAG_IMMUTABLE or FLAG_UPDATE_CURRENT)
+            }
+
+        mediaSession = MediaSession
+            .Builder(this, player)
+            .setSessionActivity(sessionActivityPendingIntent)
+            .build()
+    }
+
+    private inner class MediaSessionServiceListener : Listener {
+        override fun onForegroundServiceStartNotAllowedException() {
+            val notificationManagerCompat = NotificationManagerCompat.from(this@PlaybackService)
+            ensureNotificationChannel(notificationManagerCompat)
+            val pendingIntent =
+                TaskStackBuilder.create(this@PlaybackService).run {
+                    addNextIntent(Intent(this@PlaybackService, MainActivity::class.java))
+
+                    getPendingIntent(0, FLAG_IMMUTABLE or FLAG_UPDATE_CURRENT)
+                }
+            val builder =
+                NotificationCompat.Builder(this@PlaybackService, CHANNEL_ID)
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(androidx.media3.session.R.drawable.media3_notification_small_icon)
+                    .setContentTitle(getString(R.string.notification_content_title))
+                    .setStyle(
+                        NotificationCompat.BigTextStyle().bigText(getString(R.string.notification_content_text))
+                    )
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true)
+            if (ActivityCompat.checkSelfPermission(
+                    this@PlaybackService,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            notificationManagerCompat.notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+
+    private fun ensureNotificationChannel(notificationManagerCompat: NotificationManagerCompat) {
+        if (Util.SDK_INT < 26 || notificationManagerCompat.getNotificationChannel(CHANNEL_ID) != null)
+            return
+
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            getString(R.string.notification_channel_name),
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManagerCompat.createNotificationChannel(channel)
+    }
+}
