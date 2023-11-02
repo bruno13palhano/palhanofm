4d88446a5a9b17720c23296a084b3d902a1d3060
bruno13palhano <bruno13palhano@gmail.com>
2023-11-02 15:08:21 -0300

    Add system ui controller

--- app/src/main/java/com/bruno13palhano/palhanofm/ui/theme/Theme.kt
+++ app/src/main/java/com/bruno13palhano/palhanofm/ui/theme/Theme.kt
@@ -8,12 +8,15 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = md_theme_dark_primary,
@@ -94,13 +97,16 @@ fun PalhanoFMTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = colorScheme.background,
+            darkIcons = !darkTheme
+        )
+        systemUiController.setNavigationBarColor(
+            color = colorScheme.surfaceColorAtElevation(3.dp),
+            darkIcons = !darkTheme
+        )
     }
 
     MaterialTheme(
