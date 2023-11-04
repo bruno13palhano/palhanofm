6c4c2dbfa3bc7d2a12ba40ce9d378c6e68a01e33
bruno13palhano <bruno13palhano@gmail.com>
2023-11-04 00:17:51 -0300

    Add navGraph

---
+++ app/src/main/java/com/bruno13palhano/palhanofm/ui/navigation/MainNavGraph.kt
@@ -0,0 +1,45 @@
+package com.bruno13palhano.palhanofm.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.bruno13palhano.palhanofm.ui.screens.HomeScreen
+import com.bruno13palhano.palhanofm.ui.screens.SchedulesScreen
+import com.bruno13palhano.palhanofm.ui.screens.SettingsScreen
+
+@Composable
+fun MainNavGraph(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    startDestination: String = MainDestinations.HOME_ROUTE,
+    showBottomMenu: (show: Boolean) -> Unit = {},
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(route = MainDestinations.HOME_ROUTE) {
+            showBottomMenu(true)
+            HomeScreen()
+        }
+
+        composable(route = MainDestinations.SCHEDULES_ROUTE) {
+            showBottomMenu(true)
+            SchedulesScreen()
+        }
+
+        composable(route = MainDestinations.SETTINGS_ROUTE) {
+            showBottomMenu(true)
+            SettingsScreen()
+        }
+    }
+}
+
+object MainDestinations {
+    const val HOME_ROUTE = "home_route"
+    const val SCHEDULES_ROUTE = "schedules_route"
+    const val SETTINGS_ROUTE = "settings_route"
+}
