6587a75e6b0ed31761301421f743a2afe3ef1ddb
bruno13palhano <bruno13palhano@gmail.com>
2023-11-03 22:19:41 -0300

    Add bottom and drawer menu

---
+++ app/src/main/java/com/bruno13palhano/palhanofm/ui/components/Menus.kt
@@ -0,0 +1,185 @@
+package com.bruno13palhano.palhanofm.ui.components
+
+import android.content.res.Configuration
+import androidx.annotation.StringRes
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.Schedule
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.bruno13palhano.palhanofm.R
+import com.bruno13palhano.palhanofm.ui.navigation.MainDestinations
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun DrawerMenu(
+    drawerState: DrawerState,
+    navController: NavHostController,
+    content: @Composable () -> Unit
+) {
+    val items = listOf(
+        Screen.Home,
+        Screen.Schedules,
+        Screen.Settings
+    )
+    val orientation = LocalConfiguration.current.orientation
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    var selectedItem by remember { mutableStateOf(items[0]) }
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(
+                drawerShape = RectangleShape
+            ) {
+                LazyColumn {
+                    stickyHeader {
+                        Card(
+                            modifier = Modifier.padding(bottom = 16.dp),
+                            shape = RectangleShape
+                        ) {
+                            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                                Image(
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                        .fillMaxWidth()
+                                        .sizeIn(minHeight = 208.dp),
+                                    imageVector = Icons.Filled.Image,
+                                    contentDescription = stringResource(id = R.string.app_logo_label)
+                                )
+                            }
+                            Text(
+                                modifier = Modifier
+                                    .padding(vertical = 16.dp)
+                                    .fillMaxWidth(),
+                                text = stringResource(id = R.string.app_name),
+                                textAlign = TextAlign.Center,
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                        }
+                    }
+                    items(
+                        items = items
+                    ) { screen ->
+                        NavigationDrawerItem(
+                            icon = {
+                                Icon(imageVector = screen.icon, contentDescription = null)
+                            },
+                            label = {
+                                Text(text = stringResource(id = screen.resourceId))
+                            },
+                            selected = currentDestination?.hierarchy?.any { destination ->
+                                destination.route == screen.route
+                            } == true,
+                            onClick = {
+                                selectedItem = screen
+                                navController.navigate(screen.route) {
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                    restoreState = true
+                                    launchSingleTop = true
+                                }
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                        )
+                    }
+                }
+            }
+        },
+        content = content
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun DrawerPreview() {
+    DrawerMenu(
+        navController = rememberNavController(),
+        drawerState = rememberDrawerState(initialValue = DrawerValue.Open),
+        content = {}
+    )
+}
+
+@Composable
+fun BottomMenu(navController: NavController) {
+    val items = listOf(
+        Screen.Home,
+        Screen.Schedules,
+        Screen.Settings
+    )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    NavigationBar {
+        items.forEach { screen ->
+            NavigationBarItem(
+                icon = { Icon(imageVector = screen.icon, contentDescription = null) },
+                label = { Text(text = stringResource(id = screen.resourceId)) },
+                selected = currentDestination?.hierarchy?.any{ it.route == screen.route } == true,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+sealed class Screen(val route: String, val icon: ImageVector, @StringRes val resourceId: Int) {
+    object Home: Screen(MainDestinations.HOME_ROUTE, Icons.Filled.Home, R.string.home_label)
+    object Schedules: Screen(MainDestinations.SCHEDULES_ROUTE, Icons.Filled.Schedule, R.string.schedules_label)
+    object Settings: Screen(MainDestinations.SETTINGS_ROUTE, Icons.Filled.Settings, R.string.settings_label)
+}
