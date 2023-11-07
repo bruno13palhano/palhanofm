182bda37563f61926dd79801c82c3364472ade0a
bruno13palhano <bruno13palhano@gmail.com>
2023-11-07 14:13:00 -0300

    Add a hilt module to inject Repositories

---
+++ core/src/main/java/com/bruno13palhano/core/di/RepositoryModule.kt
@@ -0,0 +1,24 @@
+package com.bruno13palhano.core.di
+
+import com.bruno13palhano.core.SponsorsData
+import com.bruno13palhano.core.model.Sponsor
+import com.bruno13palhano.core.repository.SponsorsRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Qualifier
+annotation class SponsorsRep
+
+@InstallIn(SingletonComponent::class)
+@Module
+internal abstract class RepositoryModule {
+
+    @SponsorsRep
+    @Singleton
+    @Binds
+    abstract fun bindSponsorsRepository(repository: SponsorsRepository): SponsorsData<Sponsor>
+}
