68053ad9064d3c223558430398850eda8a0d51e3
bruno13palhano <bruno13palhano@gmail.com>
2023-11-07 19:22:06 -0300

    Remove unused method

--- core/src/main/java/com/bruno13palhano/core/network/SponsorsNetwork.kt
+++ core/src/main/java/com/bruno13palhano/core/network/SponsorsNetwork.kt
@@ -1,7 +1,6 @@
 package com.bruno13palhano.core.network
 
 import com.bruno13palhano.core.model.Sponsor
-import kotlinx.coroutines.flow.Flow
 
 internal interface SponsorsNetwork {
     suspend fun getAll(): List<Sponsor>
