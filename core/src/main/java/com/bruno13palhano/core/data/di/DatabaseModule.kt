4bc3327bb27b06bf1472c6e69047e99bd6fec0ab
bruno13palhano <bruno13palhano@gmail.com>
2023-11-07 14:09:58 -0300

    Add a provides for the database

---
+++ core/src/main/java/com/bruno13palhano/core/data/di/DatabaseModule.kt
@@ -0,0 +1,34 @@
+package com.bruno13palhano.core.data.di
+
+import android.content.Context
+import cache.SponsorsTableQueries
+import com.bruno13palhano.cache.PalhanoFmDatabase
+import com.bruno13palhano.core.data.database.DatabaseFactory
+import com.bruno13palhano.core.data.database.DriverFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+internal object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabaseFactoryDriver(
+        @ApplicationContext context: Context
+    ): PalhanoFmDatabase {
+        return DatabaseFactory(
+            driverFactory = DriverFactory(context = context)
+        ).createDriver()
+    }
+
+    @Provides
+    @Singleton
+    fun providesSponsorsTable(
+        database: PalhanoFmDatabase
+    ): SponsorsTableQueries = database.sponsorsTableQueries
+}
