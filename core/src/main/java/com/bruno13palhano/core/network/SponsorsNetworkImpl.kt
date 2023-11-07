515e48265335023a3be3c6aa1dbe6ce55681f253
bruno13palhano <bruno13palhano@gmail.com>
2023-11-07 19:27:37 -0300

    Add SponsorsNetwork implementation

--- core/src/main/java/com/bruno13palhano/core/network/SponsorsNetworkImpl.kt
+++ core/src/main/java/com/bruno13palhano/core/network/SponsorsNetworkImpl.kt
@@ -1,12 +1,7 @@
 package com.bruno13palhano.core.network
 
 import com.bruno13palhano.core.model.Sponsor
-import com.bruno13palhano.core.network.PFDispatchers.IO
 import com.bruno13palhano.core.network.model.asExternal
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import java.lang.Exception
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -14,13 +9,11 @@ import javax.inject.Singleton
 @Singleton
 internal class SponsorsNetworkImpl @Inject constructor(
     private val apiService: Service,
-    @Dispatcher(IO) private val dispatcher: CoroutineDispatcher
 ) : SponsorsNetwork {
-    override fun getAll(): Flow<List<Sponsor>> = flow {
+    override suspend fun getAll(): List<Sponsor> =
         try {
-            emit(apiService.getSponsors().map { it.asExternal() })
-        } catch (e: Exception) {
-            e.printStackTrace()
+            apiService.getSponsors().map { it.asExternal() }
+        } catch (ignored: Exception) {
+            emptyList()
         }
-    }.flowOn(dispatcher)
 }
