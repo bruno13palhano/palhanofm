2187feea809e4a56bdb4747fc84120ed3c516713
bruno13palhano <bruno13palhano@gmail.com>
2023-11-07 19:21:42 -0300

    Remove flow

--- core/src/main/java/com/bruno13palhano/core/network/SponsorsNetwork.kt
+++ core/src/main/java/com/bruno13palhano/core/network/SponsorsNetwork.kt
@@ -3,6 +3,6 @@ package com.bruno13palhano.core.network
 import com.bruno13palhano.core.model.Sponsor
 import kotlinx.coroutines.flow.Flow
 
-interface SponsorsNetwork {
-    fun getAll(): Flow<List<Sponsor>>
+internal interface SponsorsNetwork {
+    suspend fun getAll(): List<Sponsor>
 }
