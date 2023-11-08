23388a22777dde41f5a4acd63437a9bcb4e39a1f
bruno13palhano <bruno13palhano@gmail.com>
2023-11-08 01:50:18 -0300

    Add a bind to inject SchedulesLight

--- core/src/main/java/com/bruno13palhano/core/repository/di/InternalDataModule.kt
+++ core/src/main/java/com/bruno13palhano/core/repository/di/InternalDataModule.kt
@@ -1,7 +1,10 @@
 package com.bruno13palhano.core.repository.di
 
+import com.bruno13palhano.core.model.Schedule
 import com.bruno13palhano.core.repository.sponsors.SponsorsData
 import com.bruno13palhano.core.model.Sponsor
+import com.bruno13palhano.core.repository.schedules.SchedulesData
+import com.bruno13palhano.core.repository.schedules.SchedulesLight
 import com.bruno13palhano.core.repository.sponsors.SponsorsLight
 import dagger.Binds
 import dagger.Module
@@ -13,6 +16,9 @@ import javax.inject.Singleton
 @Qualifier
 internal annotation class InternalSponsorsLight
 
+@Qualifier
+internal annotation class InternalSchedulesLight
+
 @InstallIn(SingletonComponent::class)
 @Module
 internal abstract class InternalDataModule {
@@ -21,4 +27,9 @@ internal abstract class InternalDataModule {
     @Singleton
     @Binds
     abstract fun bindInternalSponsorsLight(data: SponsorsLight): SponsorsData<Sponsor>
+
+    @InternalSchedulesLight
+    @Singleton
+    @Binds
+    abstract fun bindInternalSchedulesLight(data: SchedulesLight): SchedulesData<Schedule>
 }
