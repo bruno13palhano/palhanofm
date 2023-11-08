6a65cd70d52ea04ae4a0c94cc75f9bee060d87db
bruno13palhano <bruno13palhano@gmail.com>
2023-11-08 01:12:52 -0300

    Add a method to get schedules from serve

--- core/src/main/java/com/bruno13palhano/core/network/Service.kt
+++ core/src/main/java/com/bruno13palhano/core/network/Service.kt
@@ -1,5 +1,6 @@
 package com.bruno13palhano.core.network
 
+import com.bruno13palhano.core.network.model.ScheduleNet
 import com.bruno13palhano.core.network.model.SponsorNet
 import retrofit2.http.GET
 
@@ -7,4 +8,7 @@ internal interface Service {
 
     @GET("sponsors/all")
     suspend fun getSponsors(): List<SponsorNet>
+
+    @GET("schedules/all")
+    suspend fun getSchedules(): List<ScheduleNet>
 }
