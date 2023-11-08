89377ec4a7044b3d73551bf5c39270e2d2493617
bruno13palhano <bruno13palhano@gmail.com>
2023-11-08 01:11:08 -0300

    Refactor

--- core/src/main/java/com/bruno13palhano/core/network/sponsors/SponsorsNetworkImpl.kt
+++ core/src/main/java/com/bruno13palhano/core/network/sponsors/SponsorsNetworkImpl.kt
@@ -5,16 +5,13 @@ import com.bruno13palhano.core.network.Service
 import com.bruno13palhano.core.network.model.asExternal
 import java.lang.Exception
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 internal class SponsorsNetworkImpl @Inject constructor(
-    private val apiService: Service,
+    private val apiService: Service
 ) : SponsorsNetwork {
-    override suspend fun getAll(): List<Sponsor> =
-        try {
+    override suspend fun getAll(): List<Sponsor> {
+        return try {
             apiService.getSponsors().map { it.asExternal() }
-        } catch (ignored: Exception) {
-            emptyList()
-        }
+        } catch (ignored: Exception) { emptyList() }
+    }
 }
