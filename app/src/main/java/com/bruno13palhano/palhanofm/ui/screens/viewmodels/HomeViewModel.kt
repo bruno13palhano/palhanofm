252dd3649d443969ff6724d867b0e56ea259d97b
bruno13palhano <bruno13palhano@gmail.com>
2023-11-07 22:35:23 -0300

    Get Sponsors from database

--- app/src/main/java/com/bruno13palhano/palhanofm/ui/screens/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/bruno13palhano/palhanofm/ui/screens/viewmodels/HomeViewModel.kt
@@ -2,25 +2,31 @@ package com.bruno13palhano.palhanofm.ui.screens.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.core.model.Sponsor
+import com.bruno13palhano.core.repository.di.SponsorsRep
+import com.bruno13palhano.core.repository.sponsors.SponsorsDataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): ViewModel() {
-    val sponsorsImagesUrl = MutableStateFlow(
-        listOf(
-            "https://www.palhanofm.adcast.com.br/admin/assets/img/anunciantes/1819-2021-03-26.png",
-            "https://www.palhanofm.adcast.com.br/admin/assets/img/anunciantes/1868-2023-10-05.jpg",
-            "https://www.palhanofm.adcast.com.br/admin/assets/img/anunciantes/8344-2023-10-05.jpg",
-            "https://www.palhanofm.adcast.com.br/admin/assets/img/anunciantes/7570-2023-10-05.png"
-        )
-    )
+class HomeViewModel @Inject constructor(
+    @SponsorsRep private val repository: SponsorsDataRepository<Sponsor>
+): ViewModel() {
+    val sponsorsImagesUrl = repository.getAll()
+        .map { it.map { sponsor -> sponsor.imageUrl } }
         .stateIn(
             scope = viewModelScope,
             started = WhileSubscribed(5_000),
             initialValue = emptyList()
         )
+
+    fun synchronize() {
+        viewModelScope.launch {
+            repository.synchronize()
+        }
+    }
 }
