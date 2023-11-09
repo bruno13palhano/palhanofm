a7f460f970ff59d5b1d40421594a0ed03c256883
bruno13palhano <bruno13palhano@gmail.com>
2023-11-08 23:55:46 -0300

    Refactor

--- app/src/main/java/com/bruno13palhano/palhanofm/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/bruno13palhano/palhanofm/ui/screens/HomeScreen.kt
@@ -4,15 +4,18 @@ import android.content.ComponentName
 import android.content.Context
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Whatsapp
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -20,24 +23,34 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.media3.common.Player
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
 import androidx.media3.ui.PlayerView
+import coil.compose.rememberAsyncImagePainter
 import com.bruno13palhano.palhanofm.R
 import com.bruno13palhano.palhanofm.mediaplayer.PlaybackService
+import com.bruno13palhano.palhanofm.ui.components.Carousel
+import com.bruno13palhano.palhanofm.ui.screens.viewmodels.HomeViewModel
 import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.MoreExecutors
 
@@ -46,12 +59,18 @@ private val controller: MediaController?
     get() = if (controllerFuture.isDone) controllerFuture.get() else null
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+    LaunchedEffect(key1 = Unit) { viewModel.synchronize() }
+
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
     var isVisible by remember { mutableStateOf(false) }
 
-    DisposableEffect(key1 = lifecycleOwner, key2 = context) {
+    val sponsorsUrlImages by viewModel.sponsorsImagesUrl.collectAsStateWithLifecycle()
+
+    DisposableEffect(key1 = lifecycleOwner) {
         val observer = LifecycleEventObserver { _, event ->
             if (event == Lifecycle.Event.ON_START) {
                 initializeController(context) {
@@ -71,8 +90,54 @@ fun HomeScreen() {
         }
     }
 
-    if (isVisible) {
-        PlayerContent(context = context)
+    Scaffold(
+        floatingActionButton = {
+            ExtendedFloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Whatsapp,
+                    contentDescription = stringResource(id = R.string.request_song_description)
+                )
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    text = stringResource(id = R.string.hello_label)
+                )
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            if (isVisible) {
+                PlayerContent(context = context)
+                if (sponsorsUrlImages.isNotEmpty()) {
+                    Carousel(
+                        modifier = Modifier
+                            .weight(1F)
+                            .fillMaxWidth(),
+                        pageCount = sponsorsUrlImages.size
+                    ) { page ->
+                        ElevatedCard(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(start = 8.dp, top = 16.dp, end = 8.dp, bottom = 8.dp)
+                        ) {
+                            Image(
+                                painter = rememberAsyncImagePainter(model = sponsorsUrlImages[page]),
+                                contentDescription = stringResource(id = R.string.current_sponsor_description),
+                                contentScale = ContentScale.Inside,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(16.dp)
+                                    .clip(RoundedCornerShape(5))
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -81,71 +146,60 @@ fun HomeScreen() {
 private fun PlayerContent(
     context: Context
 ) {
-    Scaffold(
-        floatingActionButton = {
-            ExtendedFloatingActionButton(onClick = {  }) {
-                Text(
-                    modifier = Modifier.padding(8.dp),
-                    text = stringResource(id = R.string.hello_label)
-                )
-                Icon(
-                    imageVector = Icons.Filled.Whatsapp,
-                    contentDescription = stringResource(id = R.string.request_song_description)
+    var title by remember { mutableStateOf("") }
+    val arts = listOf(
+        context.getDrawable(R.drawable.player_image_1),
+        context.getDrawable(R.drawable.player_image_2),
+        context.getDrawable(R.drawable.player_image_3)
+    )
+    var currentId = 0
+
+    AndroidView(
+        modifier = Modifier.fillMaxWidth().sizeIn(maxHeight = 408.dp),
+        factory = {
+            PlayerView(context).apply {
+                showController()
+                useController = true
+                player = controller
+                layoutParams = FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
                 )
             }
-        }
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(it)
-                .verticalScroll(rememberScrollState())
-        ) {
-            var title by remember { mutableStateOf("") }
-
-            AndroidView(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .sizeIn(maxHeight = 304.dp),
-                factory = {
-                    PlayerView(context).apply {
-                        showController()
-                        useController = true
-                        player = controller
-                        layoutParams = FrameLayout.LayoutParams(
-                            ViewGroup.LayoutParams.MATCH_PARENT,
-                            ViewGroup.LayoutParams.MATCH_PARENT
-                        )
-                        defaultArtwork = context.getDrawable(R.drawable.palhano_fm_logo)
+        },
+        update = { playerView ->
+            playerView.player?.addListener(object : Player.Listener {
+                override fun onEvents(player: Player, events: Player.Events) {
+                    val newTitle = player.mediaMetadata.title.toString()
+                    if(title != newTitle) {
+                        val nextId = (currentId + 1) % arts.size
+                        playerView.defaultArtwork = arts[nextId]
+                        currentId = nextId
                     }
-                },
-                update = { playerView ->
-                    playerView.player?.addListener(object : Player.Listener {
-                        override fun onEvents(player: Player, events: Player.Events) {
-                            title = player.mediaMetadata.title.toString()
 
-                            if (events.contains(Player.EVENT_IS_PLAYING_CHANGED)) {
-                                if (!player.isPlaying) {
-                                    player.seekToDefaultPosition()
-                                }
-                            }
+                    title = newTitle
+                    playerView.artworkDisplayMode = PlayerView.ARTWORK_DISPLAY_MODE_FILL
+
+                    if (events.contains(Player.EVENT_IS_PLAYING_CHANGED)) {
+                        if (!player.isPlaying) {
+                            player.seekToDefaultPosition()
                         }
-                    })
+                    }
                 }
-            )
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                text = title,
-                style = MaterialTheme.typography.titleLarge
-            )
+            })
         }
-    }
+    )
+    Text(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        text = title,
+        fontWeight = FontWeight.Bold,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 1,
+        style = MaterialTheme.typography.titleMedium
+    )
 }
 
-private fun releaseController() {
-    MediaController.releaseFuture(controllerFuture)
-}
+private fun releaseController() = MediaController.releaseFuture(controllerFuture)
 
 @androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
 private fun initializeController(context: Context, launched: () -> Unit) {
